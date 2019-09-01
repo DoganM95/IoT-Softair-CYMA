0e0dc6c3bb728c39417c5a27248f9774c369beeb
DoganM95 <doganmermertas@hotmail.de>
2019-09-02 01:41:42 +0200

    added webOTA capability after login, fixed logics

--- ESP32/Sketch.ino
+++ ESP32/Sketch.ino
@@ -1,10 +1,22 @@
 // Libraries
+#include <ArduinoOTA.h>
+#include <ESPmDNS.h>
+#include <Update.h>
+#include <WebServer.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
-
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
 #include "./libraries/newlib/pthread.h"
 
 // Credentials
+#include "Credentials/OtaLogin.h"
+#include "Credentials/Wifi.h"
+
+// ----------------------------------------------------------------------------
+// SETTINGS
+// ----------------------------------------------------------------------------
+int otaPort = 80;
 
 // GPIO
 // input only
@@ -24,39 +36,132 @@ int triggerDebounceDelay = 120;
 int shotNumber = 0;
 int triggerNumber = 0;
 
+// WebServer
+// allows you to set the realm of authentication Default:"Login Required"
+const char* www_realm = "Custom Auth Realm";
+// the Content of the HTML response in case of Unautherized Access Default:empty
+String authFailResponse = "Authentication Failed";
+
+WebServer server(otaPort);
+const char* serverIndex =
+    "<form method='POST' action='/update' enctype='multipart/form-data'><input "
+    "type='file' name='update'><input type='submit' value='Update'></form>";
+
+// ----------------------------------------------------------------------------
+//  Function Declarations
+// ----------------------------------------------------------------------------
+void shoot();
+
+// ----------------------------------------------------------------------------
+// SETUP
+// ----------------------------------------------------------------------------
 void setup() {
-  // Serial Setup
+  // Serial SETUP
   Serial.begin(115200);
 
-  // GPIO Setup
+  // GPIO SETUP
 
   // shotsensor
   ledcSetup(shotChannel, freq, resolution);
   ledcAttachPin(shotSensorPin, shotChannel);
-  pinMode(shotSensorPin, INPUT);
-
-  // trigger switch
-  pinMode(triggerPin, INPUT);
-
-  //fire pin
+  pinMode(shotSensorPin, INPUT_PULLDOWN);
+  pinMode(triggerPin, INPUT_PULLDOWN);
   pinMode(firePin, OUTPUT);
+
+  // Wifi Setup
+  Serial.println("Waiting for Wifi.");
+  WiFi.begin(WIFI_SSID, WIFI_PASSORD);
+  while (WiFi.status() != WL_CONNECTED) {  // Wait for Wifi connection
+  }
+  Serial.printf("Connected to Wifi: %s", WIFI_SSID);
+
+  // Webserver for OTA Updates
+  MDNS.begin(WIFI_HOSTNAME);
+  ArduinoOTA.begin();
+
+  server.on("/", []() {
+    if (!server.authenticate(www_username, www_password))
+    // Basic Auth Method with Custom realm and Failure Response
+    // return server.requestAuthentication(BASIC_AUTH, www_realm,
+    // authFailResponse); Digest Auth Method with realm="Login Required" and
+    // empty Failure Response return server.requestAuthentication(DIGEST_AUTH);
+    // Digest Auth Method with Custom realm and empty Failure Response
+    // return server.requestAuthentication(DIGEST_AUTH, www_realm);
+    // Digest Auth Method with Custom realm and Failure Response
+    {
+      return server.requestAuthentication(DIGEST_AUTH, www_realm,
+                                          authFailResponse);
+    }
+    server.sendHeader("Connection, lel", "close");
+    server.send(200, "text/html", serverIndex);
+
+    server.on("/", HTTP_GET, []() {
+      server.sendHeader("Connection", "close");
+      server.send(200, "text/html", serverIndex);
+    });
+    server.on(
+        "/update", HTTP_POST,
+        []() {
+          server.sendHeader("Connection", "close");
+          server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+          ESP.restart();
+        },
+        []() {
+          HTTPUpload& upload = server.upload();
+          if (upload.status == UPLOAD_FILE_START) {
+            Serial.setDebugOutput(true);
+            Serial.printf("Update: %s\n", upload.filename.c_str());
+            if (!Update.begin()) {  // start with max available size
+              Update.printError(Serial);
+            }
+          } else if (upload.status == UPLOAD_FILE_WRITE) {
+            if (Update.write(upload.buf, upload.currentSize) !=
+                upload.currentSize) {
+              Update.printError(Serial);
+            }
+          } else if (upload.status == UPLOAD_FILE_END) {
+            if (Update.end(
+                    true)) {  // true to set the size to the current progress
+              Serial.printf("Update Success: %u\nRebooting...\n",
+                            upload.totalSize);
+            } else {
+              Update.printError(Serial);
+            }
+            Serial.setDebugOutput(false);
+          } else {
+            Serial.printf(
+                "Update Failed Unexpectedly (likely broken connection): "
+                "status=%d\n",
+                upload.status);
+          }
+        });
+  });
+  server.begin();
+  MDNS.addService("http", "tcp", otaPort);
+  Serial.printf("Open http://%s:%i", WiFi.localIP(), otaPort);
 }
 
+// ----------------------------------------------------------------------------
+// MAIN LOOP
+// ----------------------------------------------------------------------------
 void loop() {
   Serial.println("entering sensor loop now");
   while (true) {
-    shoot();
+    ArduinoOTA.handle();
+    server.handleClient();
     if (digitalRead(triggerPin) == 1) {
       delay(triggerDebounceDelay / 2);
+
       if (digitalRead(triggerPin) == 1) {
         delay(triggerDebounceDelay / 2);
         Serial.print("Trigger Pulled - ");
         Serial.println(++triggerNumber);
         while (digitalRead(triggerPin) == 1) {
-          // Shoot
+          shoot(true);
         }
-        delay(triggerDebounceDelay / 8);
+        delay(triggerDebounceDelay / 4);
         if (digitalRead(triggerPin) == 0) {
+          shoot(false);
           continue;
         }
       }
@@ -64,15 +169,17 @@ void loop() {
   }
 }
 
-void shoot() {
-  if (digitalRead(shotSensorPin) == 0) {
-    Serial.print("Shot FIred!!! - ");
-    Serial.println(++shotNumber);
-
-    while (digitalRead(34) == 0) {
-    }
+// ----------------------------------------------------------------------------
+// Actions
+// ----------------------------------------------------------------------------
+void shoot(boolean state) {
+  if (state) {
+    digitalWrite(firePin, 1);
+  } else {
+    digitalWrite(firePin, 0);
   }
 }
+
 // Todo:
 
 // pthread: Run loops threaded (like shot detection loop)
@@ -90,3 +197,6 @@ void shoot() {
 
 // mechanically integrate a metal (flat side of a screw?) into trigger which
 // gets touched wen touching the trigger to wake the esp from deepsleep mode
+
+// integrate led's to look like a tritium sight (with configurable brightness?
+// using photoresistor?)
