491c776fdb49cdc0829e3d0f2cbdc0f02ac9759f
Dogan <38842553+DoganM95@users.noreply.github.com>
2020-02-03 00:39:37 +0100

    Delete Wifi.h

--- ESP32/Software/Credentials/Wifi.h
+++
@@ -1,8 +0,0 @@
-// Wifi
-char* WIFI_SSID = "HalfByte";
-char* WIFI_PASSORD = "18811938";
-char* WIFI_HOSTNAME = "CYMA-Glock18C---ESP32";
-
-// char* WIFI_SSID = "TP-Link_2224";
-// char* WIFI_PASSORD = "78642348";
-// char* WIFI_HOSTNAME = "CYMA-Glock18C---ESP32";
